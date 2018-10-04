f413ea6c9cd94632e53a6ba0a274420b6915b098
Marcus Assis (linux-mint) <assis88@gmail.com>
2018-10-04 01:19:15 -0300

    Consertando erro na página consultar setores

--- banco_de_dados/comum/read-setores.php
+++ banco_de_dados/comum/read-setores.php
@@ -15,15 +15,20 @@ while ($registros = $querySelect->fetch_assoc()){
 
     if ($situacao == "I") {
         echo "<tr id='destaque' style='background-color: rgba(174,35,38,0.44)'>";
+        echo "<td>$nome</td><td><a href=''></a></td><td>$responsavel</td><td>$telefone</td>";
+        echo "<td><a href='editar-setor.php?id=$id'><i class='material-icons'>edit</i></a></td>";
+        echo "<td><a href='../../banco_de_dados/comum/modifica_situacao_setor.php?id=$id&sit_setor=$situacao'><i class='material-icons' style='color:red'>do_not_disturb_on</i></a></td>";
+        echo "</tr>";
     }
     else{
         echo "<tr id='destaque'>";
-    }
-
         echo "<td>$nome</td><td><a href=''></a></td><td>$responsavel</td><td>$telefone</td>";
         echo "<td><a href='editar-setor.php?id=$id'><i class='material-icons'>edit</i></a></td>";
         echo "<td><a href='../../banco_de_dados/comum/modifica_situacao_setor.php?id=$id&sit_setor=$situacao'><i class='material-icons' style='color:green'>lens</i></a></td>";
         echo "</tr>";
+    }
+
+
 
 }
 
