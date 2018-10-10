e1d69979a8e781820e6ba68d91a97ebd9395d276
Marcus Assis (SEAP) <assis88@gmail.com>
2018-10-10 14:02:31 -0300

    Inicio das modificacoes do tratamento dos arquivos

--- _paginas/comum/relatorio-setores.php
+++ _paginas/comum/relatorio-setores.php
@@ -6,22 +6,21 @@
         <div class="col s12">
             <h5 class="light">Itens do Setor</h5>
             <hr>
-            <div id="impressao">
+            <div id="impressao" style="width: 75%">
                 <link rel="stylesheet" href="../../_materialize/css/materialize.min.css">
                 <link rel="stylesheet" href="../../_css/comum.css">
                 <link rel="stylesheet" href="https://jqueryvalidation.org/files/demo/site-demos.css">
-                <table class="striped">
+                <table class="striped" >
                     <thead>
                         <tr>
                             <th colspan="3"><p class="center-align">SECRETARIA DE ADMINISTRAÇÃO PENITENCIÁRIA E RESSOCIALIZAÇÃO</p><p class="center-align">COORDENAÇÃO DE MATERIAL E PATRIMÔNIO - COMAP</p></th>
-                            <th colspan="2" class="center-align"><img src="../../_imagens/brasao-bahia.jpg" style="width: 50%"></th>
+                            <th colspan="1" class="center-align"><img src="../../_imagens/brasao-bahia.jpg" style="width: 50%"></th>
                         </tr>
                         <tr class="font_tabela_cab">
-                            <th style="width: 6%">ITEM</th>
-                            <th style="width: 26%">NOME</th>
-                            <th style="width: 47%">DESCRIÇÃO</th>
-                            <th style="width: 12%">Nº DE PATRIMÔNIO</th>
-                            <th style="width: 8%">SIT. FÍSICA</th>
+                            <th style="width: 10%">ITEM</th>
+                            <th style="width: 30%">NOME</th>
+                            <th style="width: 20%">Nº DE PATRIMÔNIO</th>
+                            <th style="width: 15%">SIT. FÍSICA</th>
                         </tr>
 
                 </thead>
@@ -29,14 +28,14 @@
                 <?php include_once '../../banco_de_dados/comum/read-itens-setores.php' ?>
                 </tbody>
                     <tr>
-                        <td colspan="5"><p class="center-align font_tabela_cab">Obs.: Cabe aos funcionários deste setor a responsabilidade dos bens móveis citado acima.</p></td>
+                        <td colspan="4"><p class="center-align font_tabela_cab">Obs.: Cabe aos funcionários deste setor a responsabilidade dos bens móveis citado acima.</p></td>
                     </tr>
             </table>
                 <div class="col s4">
                     <p>Data de conferência: _____/______/__________</p>
                 </div>
                 <div class="col s4">
-                    <p style="text-decoration: overline; margin-top: 50px;">Responsável pela Conferência/COMAP</p>
+                    <p style="text-decoration: overline; margin-top: 50px;">Responsável pelo Patrimônio da Unidade</p>
                 </div>
                 <div class="col s4">
                     <p style="text-decoration: overline; margin-top: 50px;">Responsável pela Conferência/SETOR</p>
